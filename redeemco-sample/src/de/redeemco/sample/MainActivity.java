4430c7774aa7ace3ffca56bc197a5460a91c046b
Tomas Hubalek <tom.hubalek@gmail.com>
2012-07-02 09:20:48 +0200

    Slight UX improvements.

--- redeemco-sample/src/de/redeemco/sample/MainActivity.java
+++ redeemco-sample/src/de/redeemco/sample/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import de.redeemco.library.RedeemCodeValidator;
 
@@ -20,9 +21,14 @@ public class MainActivity extends Activity {
     /**
      * Put APP KEY taken from redeemco.de here.
      */
-    public static final String APP_KEY = "D9NQMDNG8AB9HDIYNR7ESBXIGCHTZS";
+//    public static final String APP_KEY = "D9NQMDNG8AB9HDIYNR7ESBXIGCHTZS";
+
+    public static final String APP_KEY = "A8QK4CKZ35Y5Z9LNW3IZ1M2HG5EBHY";
+
     private Button bApplyRedeemCode;
+
     private TextView tvStatus;
+    private ProgressBar pbLoading;
 
     /**
      * Called when Activity is created.
@@ -41,6 +47,8 @@ public class MainActivity extends Activity {
         // button used for application of redeem code
         bApplyRedeemCode = (Button) findViewById(R.id.buttonApplyRedeemcode);
 
+        // progress bar
+        pbLoading = (ProgressBar) findViewById(R.id.pbLoading);
 
         // this code handles long verification
         @SuppressWarnings("deprecated")
@@ -71,6 +79,7 @@ public class MainActivity extends Activity {
      * Puts controls to unlocked state.
      */
     public void showUnlockedState() {
+        pbLoading.setVisibility(View.GONE);
         bApplyRedeemCode.setEnabled(false);
         tvStatus.setText(R.string.status_unlocked);
     }
@@ -79,6 +88,7 @@ public class MainActivity extends Activity {
      * Puts controls to locked state.
      */
     public void showLockedState() {
+        pbLoading.setVisibility(View.GONE);
         bApplyRedeemCode.setEnabled(true);
         tvStatus.setText(R.string.status_free);
         bApplyRedeemCode.setOnClickListener(new View.OnClickListener() {
